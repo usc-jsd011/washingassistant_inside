77e23955470c00dc78935b28c016bd0cdf4ba8c0
usc-jsd011 <127707321+usc-jsd011@users.noreply.github.com>
2023-05-17 13:52:24 +1000

    it works :)

--- main.ts
+++ main.ts
@@ -11,47 +11,52 @@ function setValues () {
     if (weather_data[0] == "Wind") {
         wind = parseFloat(weather_data[1])
     }
-    if (weather_data[0] == "Moisture") {
+    if (weather_data[0] == "Water") {
         moisture = parseFloat(weather_data[1])
     }
 }
 function checkValues () {
     if (light2 < 600) {
-        basic.showString("L")
-        basic.pause(500)
-    }
-    if (humidity > 80) {
-        basic.showString("H")
-        basic.pause(500)
-    }
-    if (temperature < 15) {
-        basic.showString("T")
-        basic.pause(500)
-    }
-    if (wind > 0) {
-        basic.showString("W")
-        basic.pause(500)
-    }
-    if (moisture < 500) {
-        basic.showString("M")
-        basic.pause(500)
+        showAlert("L")
+    } else if (humidity > 80) {
+        showAlert("H")
+    } else if (temperature < 15) {
+        showAlert("T")
+    } else if (wind > 0) {
+        showAlert("W")
+    } else if (moisture < 500) {
+        showAlert("M")
     }
 }
 input.onButtonPressed(Button.A, function () {
     setTimerMode = false
     showWeatherMode = true
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
     basic.pause(time * 1000)
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
     time = 0
 })
+function showAlert (char: string) {
+    basic.showString(char)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+    resetValues()
+}
 radio.onReceivedString(function (receivedString) {
     if (showWeatherMode) {
         weather_data = receivedString.split(" ")
-        serial.writeLine("" + (weather_data[1]))
+        setValues()
+        checkValues()
     }
-    setValues()
-    checkValues()
-    resetValues()
 })
 input.onButtonPressed(Button.B, function () {
     showWeatherMode = false
@@ -77,26 +82,23 @@ function resetValues () {
     temperature = 999
     light2 = 999
     wind = 0
+    pins.digitalWritePin(DigitalPin.P1, 0)
     basic.clearScreen()
 }
 let pinValue0 = 0
 let time = 0
 let setTimerMode = false
+let moisture = 0
+let wind = 0
+let temperature = 0
+let humidity = 0
+let light2 = 0
 let weather_data: string[] = []
 let showWeatherMode = false
-let wind = 0
-let light2 = 0
-let temperature = 0
-let moisture = 0
-let humidity = 0
-humidity = 0
-moisture = 999
-temperature = 999
-light2 = 999
-wind = 0
-showWeatherMode = true
+resetValues()
 radio.setGroup(23)
 led.setBrightness(255)
+showWeatherMode = true
 basic.forever(function () {
     pinValue0 = pins.analogReadPin(AnalogPin.P0)
     while (setTimerMode) {
